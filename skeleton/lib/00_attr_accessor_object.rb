934e4a658fed8412d2e8165f9c5ea52c53bafd56
artemplv <artemplv@gmail.com>
2023-06-06 20:12:28 -0400

    complete phase 0

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,11 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      if name.to_s[0] == "@"
+        name = name.to_s[1..-1]
+      end
+      define_method(name.to_sym) { self.instance_variable_get("@#{name}") }
+      define_method("#{name.to_sym}=") { |val| self.instance_variable_set("@#{name}",val)}
+    end
   end
 end
